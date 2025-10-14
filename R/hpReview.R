e48244693ffbf1a22d56f67b29de3d22b76292c0
Matt Grant <matt.grant@cancer.org.uk>
2025-10-14 14:15:54 +0100

    Add tests and info for hpReview

--- R/hpReview.R
+++ R/hpReview.R
@@ -1,20 +1,26 @@
+#' Health Professional Last Review
+#'
+#' @param last_review_date A date to displayed as the last date the page or content was reviewed
+#'
+#' @returns A div containing a styled health professional box and the date of last review
+#' @export
+#'
+#' @examples
+#' hpReview("08 July 2025")
 hpReview <- function(last_review_date) {
-  div(
-    # span("Health professionals",
-    #      style = "background: #e2f4fd;
-    #               color: #0076b4;
-    #               display: inline-flex;
-    #               align-items: center;
-    #               padding: 8px 12px 8px 12px;"),
-    div(style = "flex: 1;
+  htmltools::div(
+    htmltools::span("Health professionals",
+         style = "background: #e2f4fd;
+                  color: #0076b4;
+                  display: inline-flex;
+                  align-items: center;
+                  padding: 8px 12px 8px 12px;"),
+    htmltools::div(style = "flex: 1;
                    align-self: stretch;"),
-    span(HTML("Last reviewed: ", last_review_date),
+    htmltools::span(htmltools::HTML("Last reviewed:", last_review_date),
          style = "color: #666666;
                   padding: 8px 12px 8px 12px;
                   text-align: right;"),
-    # span(paste0("Next review due: ", next_review_date),
-    #      style = "color: #666666;
-    #                 padding: 8px 12px 8px 12px;"),
     style = "margin-bottom: 8px;
              display: flex;")
 }
