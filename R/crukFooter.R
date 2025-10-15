f603de779a4788b9a4b609918bcf808ababb1181
Matt Grant <matt.grant@cancer.org.uk>
2025-10-15 15:12:30 +0100

    Added fs dependency and .html fragments for footer

--- R/crukFooter.R
+++ R/crukFooter.R
@@ -9,8 +9,8 @@ crukFooter <- function() {
   # Get paths using system.file()
   # cruk_logo_path <- system.file("www", "cruk-logo.svg", package = "crukShiny")
   # regulator_logo_path <- system.file("www", "logo-fundraising-regulator-reg.svg", package = "crukShiny")
-  citation_path <- system.file("markdown", "citation-text.Rmd", package = "crukShiny")
-  email_site_path <- system.file("markdown", "email-site-text.Rmd", package = "crukShiny")
+  citation_path <- system.file("markdown", "citation-text.html", package = "crukShiny")
+  email_site_path <- system.file("markdown", "email-site-text.html", package = "crukShiny")
 
   # Base64 encode the SVGs
   # cruk_logo_base64 <- base64enc::base64encode(cruk_logo_path)
@@ -20,22 +20,25 @@ crukFooter <- function() {
     class = "central-column",
     htmltools::div(
       class = "info-footer",
-      style = "padding: 16px 48px 16px 48px; border: 2px solid black; margin-bottom: 32px; margin-top: 32px;",
+      style = "padding: 16px 48px 16px 48px;
+               border: 2px solid black;
+               margin-bottom: 32px;
+               margin-top: 32px;",
       # CRUK logo
-      # htmltools::div(
-      #   htmltools::a(
-      #     htmltools::img(
-      #       src = paste0("data:image/svg+xml;base64,", cruk_logo_base64),
-      #       class = "cruk-logo-footer",
-      #       style = "margin: 8px; margin-bottom: 16px; height: 60px;"
-      #     ),
-      #     href = "https://www.cancerresearchuk.org/"
-      #   )
-      # ),
+      htmltools::div(
+        htmltools::a(
+          htmltools::img(
+            src = "crukShiny/images/cruk-logo.svg",
+            class = "cruk-logo-footer",
+            style = "margin: 8px; margin-bottom: 16px; height: 60px;"
+          ),
+          href = "https://www.cancerresearchuk.org/"
+        )
+      ),
       bslib::layout_column_wrap(
         width = 1 / 2,
-        htmltools::includeMarkdown(citation_path),
-        htmltools::includeMarkdown(email_site_path)
+        htmltools::includeHTML(citation_path),
+        htmltools::includeHTML(email_site_path)
       ),
     ),
     htmltools::hr(style = "opacity: 1;
@@ -60,8 +63,9 @@ crukFooter <- function() {
             htmltools::div(
               htmltools::a(
                 htmltools::span("Terms and conditions", style = "font-size: 0.875rem"),
-              href = "https://www.cancerresearchuk.org/terms-and-conditions"
-            ))
+                href = "https://www.cancerresearchuk.org/terms-and-conditions"
+              )
+            )
           ),
           htmltools::tags$li(
             htmltools::div(
@@ -90,14 +94,14 @@ crukFooter <- function() {
         )
       ),
     ),
-    # htmltools::a(
-    #   htmltools::img(
-    #     src = paste0("data:image/svg+xml;base64,", regulator_logo_base64),
-    #     height = "97px",
-    #     style = "margin-right: 10px; height: 51px; margin-top: 16px; margin-bottom: 16px;"
-    #   ),
-    #   href = "https://www.fundraisingregulator.org.uk/"
-    # ),
+    htmltools::a(
+      htmltools::img(
+        src = "crukShiny/images/logo-fundraising-regulator-reg.svg",
+        height = "97px",
+        style = "margin-right: 10px; height: 51px; margin-top: 16px; margin-bottom: 16px;"
+      ),
+      href = "https://www.fundraisingregulator.org.uk/"
+    ),
     htmltools::tags$address("Cancer Research UK is a registered charity in England and Wales (1089464), Scotland (SC041666), the Isle of Man (1103) and Jersey (247). A company limited by guarantee. Registered company in England and Wales (4325234) and the Isle of Man (5713F). Registered address: 2 Redman Place, London, E20 1JQ.")
   )
 }
