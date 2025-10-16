08991872b40989ef69784dc5d4a1def7977e30a0
Matt Grant <matt.grant@cancer.org.uk>
2025-10-16 14:37:57 +0100

    Added centralColumn function()

---
+++ R/centralColumn.R
@@ -0,0 +1,32 @@
+#' Central column div
+#'
+#' @param ...
+#'
+#' @returns A div that produces a responsive central column of 900px
+#' @export
+#'
+#' @examples
+#' centralColumn(
+#'   crukTitle("Page title"),
+#'   p("Some page text")
+#' )
+centralColumn <- function(...) {
+  content <- list(...)
+
+  htmltools::tagList(
+    # Call the css that contains the media queries
+    htmltools::htmlDependency(
+      name = "centralColumn",
+      version = utils::packageVersion("shinyCRUK"),
+      src = "www",
+      package = "shinyCRUK",
+      stylesheet = "css/centralColumn.css",
+      all_files = TRUE
+    ),
+    # Create the div
+    htmltools::div(
+      class = "central-column",
+      content
+    )
+  )
+}
