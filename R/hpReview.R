5f13aecf64c6bd33e20b8612bce66702463d54f1
Matt Grant <matt.grant@cancer.org.uk>
2025-10-16 14:14:50 +0100

    Added lastReviewInternal() and changed the name of hpReview to lastReviewHP()

--- R/hpReview.R
+++ R/hpReview.R
@@ -7,7 +7,8 @@
 #'
 #' @examples
 #' hpReview("08 July 2025")
-hpReview <- function(last_review_date) {
+#'
+lastReviewHP <- function(last_review_date) {
   htmltools::div(
     htmltools::span("Health professionals",
       style = "background: #e2f4fd;
@@ -21,6 +22,29 @@ hpReview <- function(last_review_date) {
                    align-self: stretch;"),
     htmltools::span(htmltools::HTML("Last reviewed:", last_review_date),
       style = "color: #666666;
+                  font-family: Poppins;
+                  padding: 8px 12px 8px 12px;
+                  text-align: right;"
+    ),
+    style = "margin-bottom: 8px;
+             display: flex;"
+  )
+}
+
+lastReviewInternal <- function(last_review_date) {
+  htmltools::div(
+    htmltools::span("Internal only",
+                    style = "background: #fff0f8;
+                  color: #cc006c;
+                  font-family: Poppins;
+                  display: inline-flex;
+                  align-items: center;
+                  padding: 8px 12px 8px 12px;"
+    ),
+    htmltools::div(style = "flex: 1;
+                   align-self: stretch;"),
+    htmltools::span(htmltools::HTML("Last reviewed:", last_review_date),
+                    style = "color: #666666;
                   font-family: Poppins;
                   padding: 8px 12px 8px 12px;
                   text-align: right;"
