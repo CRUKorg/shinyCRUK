773e626c21b55cd8e95c2b39be2fa4fbf4fc9063
Matt Grant <matt.grant@cancer.org.uk>
2025-10-16 10:55:00 +0100

    Updated crukFooter function and supporting files

--- R/crukFooter.R
+++ R/crukFooter.R
@@ -16,34 +16,33 @@ crukFooter <- function() {
     directoryPath = system.file("www", package = "shinyCRUK")
   )
 
+  # Get paths for logos and text for reuse of content
   cruk_logo_path <- "shinyCRUK/images/cruk-logo.svg"
   regulator_logo_path <- "shinyCRUK/images/logo-fundraising-regulator-reg.svg"
-
-  # Get paths for logos and text for reuse of content
-  # cruk_logo_path <- "images/cruk-logo.svg"
-  # regulator_logo_path <- "images/logo-fundraising-regulator-reg.svg"
-
 
   citation_path <- system.file("markdown", "citation-text.html", package = "shinyCRUK", mustWork = TRUE)
   email_site_path <- system.file("markdown", "email-site-text.html", package = "shinyCRUK", mustWork = TRUE)
 
   htmltools::tagList(
-    crukCSSDependency(),
+    # crukCSSDependency(),
+    htmltools::htmlDependency(
+      name = "shinyCRUK-footer",
+      version = utils::packageVersion("shinyCRUK"),
+      src = "www",
+      package = "shinyCRUK",
+      stylesheet = "css/crukFooter.css",
+      all_files = TRUE
+    ),
     htmltools::div(
       class = "central-column",
       htmltools::div(
         class = "info-footer",
-        style = "padding: 16px 48px 16px 48px;
-               border: 2px solid black;
-               margin-bottom: 32px;
-               margin-top: 32px;",
         # CRUK logo
         htmltools::div(
           htmltools::a(
             htmltools::img(
               src = cruk_logo_path,
               class = "cruk-logo-footer",
-              style = "margin: 8px; margin-bottom: 16px; height: 60px;"
             ),
             href = "https://www.cancerresearchuk.org/"
           )
@@ -54,53 +53,38 @@ crukFooter <- function() {
           htmltools::includeHTML(email_site_path)
         ),
       ),
-      htmltools::hr(style = "opacity: 1;
-                  margin-right: calc(50% - 49.5vw) !important;
-                  border: 0;
-                  border-color: #000000;
-                  border-style: solid;
-                  border-bottom-width: 0.25rem;
-                  margin: 0px;
-                  width: auto;"),
-      htmltools::h3("Together we are", htmltools::br(),
-        " beating cancer",
-        style = "padding-top: 16px; padding-bottom: 40px"
-      ),
+      htmltools::hr(class = "footer-hr"),
+      htmltools::h3(class = "footer-strapline",
+                    "Together we are", htmltools::br(),
+                    " beating cancer"
+                    ),
       htmltools::div(
         class = "terms-and-conditions",
         htmltools::div(
           htmltools::tags$ul(
             class = "terms-and-conditions-list",
             htmltools::tags$li(
-              htmltools::div(
-                htmltools::a(
-                  htmltools::span("Terms and conditions"),
-                  href = "https://www.cancerresearchuk.org/terms-and-conditions"
-                )
+              htmltools::a(class = "terms-and-conditions-link",
+                "Terms and conditions",
+                href = "https://www.cancerresearchuk.org/terms-and-conditions"
               )
             ),
             htmltools::tags$li(
-              htmltools::div(
-                htmltools::a(
-                  htmltools::span("Privacy"),
-                  href = "https://www.cancerresearchuk.org/about-us/our-organisation/privacy-statement"
-                )
+              htmltools::a(class = "terms-and-conditions-link",
+                "Privacy",
+                href = "https://www.cancerresearchuk.org/about-us/our-organisation/privacy-statement"
               )
             ),
             htmltools::tags$li(
-              htmltools::div(
-                htmltools::a(
-                  htmltools::span("Modern slavery statement"),
-                  href = "https://www.cancerresearchuk.org/about-us/our-organisation/responsible-organisation"
-                )
+              htmltools::a(class = "terms-and-conditions-link",
+                "Modern slavery statement",
+                href = "https://www.cancerresearchuk.org/about-us/our-organisation/responsible-organisation"
               )
             ),
             htmltools::tags$li(
-              htmltools::div(
-                htmltools::a(
-                  htmltools::span("Cookies"),
-                  href = "https://crukcancerintelligence.shinyapps.io/cookiespolicy"
-                )
+              htmltools::a(class = "terms-and-conditions-link",
+              "Cookies",
+                href = "https://crukcancerintelligence.shinyapps.io/cookiespolicy"
               )
             )
           )
@@ -108,9 +92,9 @@ crukFooter <- function() {
       ),
       htmltools::a(
         htmltools::img(
+          class = "regulator-logo",
           src = regulator_logo_path,
           height = "97px",
-          style = "margin-right: 10px; height: 51px; margin-top: 16px; margin-bottom: 16px;"
         ),
         href = "https://www.fundraisingregulator.org.uk/"
       ),
