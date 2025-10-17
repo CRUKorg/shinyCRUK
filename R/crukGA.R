6dea6da3183ae7c99ae0fbf2ce5cf1f26d93194d
Matt Grant <matt.grant@cancer.org.uk>
2025-10-17 14:38:09 +0100

    Formatting for reference

--- R/crukGA.R
+++ R/crukGA.R
@@ -1,4 +1,5 @@
 #' CRUK Google analytics and cookie banner
+#'
 #' Adds the necessary html and javascript to enable google analytics in a shiny app, along with the necessary cookie banner. The app should also include the crukFooter() that includes a link to our cookies policy page. The function should be called within the head html element of a shiny app.
 #'
 #' @returns Adds google analytics to a shiny dashboard along with the necessary cookie banner
